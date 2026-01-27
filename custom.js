90a274a2aff3ed1adc01c0f86d6a8b2bb29ef914
RIShovon <rakibshovon99@gamail.com>
2026-01-28 04:48:13 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'flex'
+            sidebar.style.transform = 'translateX(0)'
+        }
+        function hideSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'none'
+
+        }
